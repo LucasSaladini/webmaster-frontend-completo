8ad60f18f438284d832048d988b3f36945296b9f
Lucas Saladini <lucassaladini@hotmail.com>
2023-01-13 20:13:48 -0300

    Working on signup form

---
+++ facebook_login/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css">
+    <!-- Custom font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap" rel="stylesheet">
+    <title>Login Facebook</title>
+  </head>
+  <body>
+    <header>
+      <div class="center">
+        <div class="logo">
+          <h2>Facebook</h2>
+        </div>
+        <!-- End of logo -->
+        <div class="form-login">
+          <form action="" method="post" class="form-login">
+            <div class="form-element">
+              <p>E-mail ou Telefone</p>
+              <input type="email" name="email" id="email">
+            </div>
+            <!-- End of form-element -->
+            <div class="form-element">
+              <p>Senha:</p>
+              <input type="password" name="password" id="password">
+            </div>
+            <!-- End of form-element -->
+            <div class="form-element">
+              <input type="submit" value="Entrar" name="action">
+            </div>
+            <!-- End of form-element -->
+          </form>
+          <!-- End of form-login -->
+        </div>
+        <div class="clear"></div>
+      </div>
+      <!-- End of center -->
+    </header>
+    <section class="main">
+      <div class="center">
+        <div class="person">
+          <img src="images/1.png" alt="">
+        </div>
+        <!-- End of person -->
+        <div class="signup">
+          <h2>Abra sua conta</h2>
+          <h3>É gratuito e sempre será</h3>
+          <form action="" class="signup_form">
+            <div class="w50">
+              <input type="text" name="" id="" placeholder="Nome">
+            </div>
+            <!-- End of w50 -->
+            <div class="w50">
+              <input type="text" name="" id="" placeholder="Sobrenome">
+            </div>
+            <!-- End of w50 -->
+            <div class="w100">
+              <input type="email" name="" id="" placeholder="E-mail">
+            </div>
+            <!-- End of w100 -->
+            <div class="w100">
+              <input type="password" name="" id="" placeholder="Senha">
+            </div>
+            <!-- End of w100 -->
+            <div class="clear"></div>
+            <div class="w100">
+              <input type="submit" value="Cadastrar" name="action">
+            </div>
+            <!-- End of w100 -->
+          </form>
+          <!-- End of signup_form -->
+        </div>
+        <div class="clear"></div>
+      </div>
+      <!-- End of center -->
+    </section>
+    <!-- End of main -->
+  </body>
+</html>
