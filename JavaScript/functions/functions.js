9d89c7a100a8187cdc22210cc7540f6e7d7f8051
Lucas Saladini <lucassaladini@hotmail.com>
2023-07-02 10:52:02 -0300

    Working with functions

---
+++ JavaScript/functions/functions.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    function performOperation(num1, num2) {
+        return num1 + num2
+    }
+
+    var result = performOperation(10, 20)
+
+    console.log(result)
+}
