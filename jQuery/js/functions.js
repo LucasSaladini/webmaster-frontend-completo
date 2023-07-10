092716578734098b94c99498ca8563ab6ebbf143
Lucas Saladini <lucassaladini@hotmail.com>
2023-07-10 17:51:32 -0300

    First manipulation with jQuery

---
+++ jQuery/js/functions.js
@@ -0,0 +1,11 @@
+$(function() {
+    alert("Olá, mundo")
+})
+
+$(document).ready(function() {
+    alert("Olá, mundo do .ready")
+})
+
+$(window).on('load', function() {
+    alert("Olá, mundo do .on")
+})
