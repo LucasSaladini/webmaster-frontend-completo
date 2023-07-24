38fac0494fbbb6cc9bd11c9a83cd182927fdc381
Lucas Saladini <lucassaladini@hotmail.com>
2023-07-24 18:01:15 -0300

    Working with animations on jQuery

---
+++ jQuery/effects_and_animations/js/script.js
@@ -0,0 +1,9 @@
+$(function() {
+    /*$('.box1').fadeOut(2000, function() {
+        $('.box2').fadeIn(3000)
+    })*/
+
+    $('.box1').click(function() {
+        $('.box2').slideToggle()
+    })
+})
