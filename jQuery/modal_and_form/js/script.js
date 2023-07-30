2f7ee88683ac84628f72269ba1eafdb1b4b56759
Lucas Saladini <lucassaladini@hotmail.com>
2023-07-30 15:28:21 -0300

    Working on validations on form

--- jQuery/modal_and_form/js/script.js
+++ jQuery/modal_and_form/js/script.js
@@ -20,4 +20,36 @@ $(function() {
             e.stopPropagation()
         })
     }
+
+    $('form#register').submit(function(e) {
+        e.preventDefault()
+
+        var name = $('input[name=name').val()
+        var phone = $('input[name=phone').val()
+        var email = $('input[name=email').val()
+
+        //Contando a quantidade de espaços e os respectivos valores
+        var amount = name.split(' ').length
+        var splitStr = name.split(' ')
+
+        if(amount >= 2) {
+            for(var i = 0; i < amount; i++) {
+                if(splitStr[i].match(/^[A-Z]{1}[a-z]{1,}$/)) {
+                    console.log('Nossa condição bateu, continuar')
+                } else {
+                    applyInvalidField($('input[name=name'))
+                    return false
+                }
+            }
+        } else {
+            applyInvalidField($('input[name=name'))
+            return false
+        }
+
+        function applyInvalidField(el) {
+            el.css('border', '2px solid red')
+            el.val('Campo inválido')
+            el.data('invalid', 'true')
+        }
+    })
 })
