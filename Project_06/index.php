c430025fbce90d038aa798cbfa95d79d78210339
Lucas Saladini <lucassaladini@hotmail.com>
2023-10-29 10:42:07 -0300

    Initial development

---
+++ Project_06/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css_fa/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Projeto 06</title>
+</head>
+<body>
+    <div class="icons-social">
+        <a href="https://facebook.com"><i class="fa-brands fa-facebook-f"></i></a>
+        <a href="https://instagram.com"><i class="fa-brands fa-instagram"></i></a>
+    </div>
+    <!-- icons-social -->
+    <section class="main">
+        <div class="center">
+            <header>
+                <div class="logo">
+                    Projeto 06
+                </div>
+                <!-- logo -->
+                <nav class="desktop">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Sobre</a></li>
+                        <li><a href="#">Contato</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+        <!-- center -->
+    </section>
+    <!-- main -->
+</body>
+</html>
