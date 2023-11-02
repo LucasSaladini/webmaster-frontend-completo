3a70305e77a305e4e07136722d458a189c82fede
Lucas Saladini <lucassaladini@hotmail.com>
2023-11-02 09:38:35 -0300

    Created box with plan details and working on new section

--- Project_06/index.php
+++ Project_06/index.php
@@ -56,5 +56,92 @@
         <!-- center -->
     </section>
     <!-- main -->
+    <section class="plans">
+        <div class="call_plans">
+            <div class="center">
+                <h2>Confira nossos planos</h2>
+            </div>
+            <!-- center -->
+        </div>
+        <!-- call_plans -->
+        <section class="plans_box">
+            <div class="center">
+                <div class="plans_box_single">
+                    <div class="plans_box_wrapper">
+                        <h2>Lorem Ipsum</h2>
+                        <div class="plan_details">
+                            <p><i class="fa fa-check-square"></i> Consectetur eiusmod pariatur consequat quis labore anim esse Lorem quis.</p>
+                            <p><i class="fa fa-check-square"></i> Consequat et dolor commodo dolor mollit laboris culpa ea eu sit dolor laboris labore.</p>
+                            <p><i class="fa fa-check-square"></i> Anim aliqua dolor qui minim fugiat mollit duis pariatur ad excepteur consequat qui eiusmod velit.</p>
+                            <p><i class="fa fa-check-square"></i> Do eiusmod ullamco magna eu magna mollit velit laborum.</p>
+                        </div>
+                        <!-- plan_details -->
+                        <div class="price">
+                            <h2>R$250,00</h2>
+                        </div>
+                        <!-- price -->
+                    </div>
+                    <!-- plans_box_wrapper -->
+                </div>
+                <!-- plans_box_single -->
+                <div class="plans_box_single">
+                    <div class="plans_box_wrapper">
+                        <h2>Lorem Ipsum</h2>
+                        <div class="plan_details">
+                            <p><i class="fa fa-check-square"></i> Consectetur eiusmod pariatur consequat quis labore anim esse Lorem quis.</p>
+                            <p><i class="fa fa-check-square"></i> Consequat et dolor commodo dolor mollit laboris culpa ea eu sit dolor laboris labore.</p>
+                            <p><i class="fa fa-check-square"></i> Anim aliqua dolor qui minim fugiat mollit duis pariatur ad excepteur consequat qui eiusmod velit.</p>
+                            <p><i class="fa fa-check-square"></i> Do eiusmod ullamco magna eu magna mollit velit laborum.</p>
+                        </div>
+                        <!-- plan_details -->
+                        <div class="price">
+                            <h2>R$250,00</h2>
+                        </div>
+                        <!-- price -->
+                    </div>
+                    <!-- plans_box_wrapper -->
+                </div>
+                <!-- plans_box_single -->
+                <div class="plans_box_single">
+                    <div class="plans_box_wrapper">
+                        <h2>Lorem Ipsum</h2>
+                        <div class="plan_details">
+                            <p><i class="fa fa-check-square"></i> Consectetur eiusmod pariatur consequat quis labore anim esse Lorem quis.</p>
+                            <p><i class="fa fa-check-square"></i> Consequat et dolor commodo dolor mollit laboris culpa ea eu sit dolor laboris labore.</p>
+                            <p><i class="fa fa-check-square"></i> Anim aliqua dolor qui minim fugiat mollit duis pariatur ad excepteur consequat qui eiusmod velit.</p>
+                            <p><i class="fa fa-check-square"></i> Do eiusmod ullamco magna eu magna mollit velit laborum.</p>
+                        </div>
+                        <!-- plan_details -->
+                        <div class="price">
+                            <h2>R$250,00</h2>
+                        </div>
+                        <!-- price -->
+                    </div>
+                    <!-- plans_box_wrapper -->
+                </div>
+                <!-- plans_box_single -->
+            </div>
+            <!-- center -->
+        </section>
+        <!-- box_plans -->
+    </section>
+    <section class="parallax">
+        <div class="center">
+            <h2>Sobre nossa <span>empresa</span></h2>
+            <p>Nisi anim enim quis non est non. 
+                Fugiat ad anim aliqua nostrud ut occaecat exercitation enim deserunt est anim ex. 
+                Id esse aute voluptate aliquip sint aliqua enim enim irure eiusmod tempor consectetur sint. 
+                Amet dolor mollit minim quis est ex incididunt officia ad. 
+                Quis minim tempor consequat anim irure cupidatat nisi sit officia cillum consectetur anim elit. 
+                Tempor elit esse irure eiusmod laboris cupidatat minim nulla fugiat et sit.
+                Culpa ipsum anim laborum do cillum aliquip adipisicing consequat esse. 
+                Nostrud Lorem elit adipisicing ut esse sit fugiat pariatur excepteur nisi veniam ad commodo. 
+                Ut cupidatat officia mollit amet dolor esse esse dolor quis fugiat anim fugiat velit veniam. 
+                Dolore sint voluptate et sit. Nostrud anim eiusmod ipsum Lorem proident ut labore pariatur irure eiusmod duis esse Lorem. 
+                Velit sunt ipsum consequat fugiat cillum anim.</p>
+        </div>
+        <!-- center -->
+    </section>
+    <!-- parallax -->
 </body>
 </html>
