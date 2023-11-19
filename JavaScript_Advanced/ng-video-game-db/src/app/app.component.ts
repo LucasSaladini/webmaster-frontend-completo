52a681c63c88af326b1ed3637a7da56812ef71ec
Lucas Saladini <lucassaladini@hotmail.com>
2023-11-19 11:19:24 -0300

    Initial commit

--- JavaScript_Advanced/ng-video-game-db/src/app/app.component.ts
+++ JavaScript_Advanced/ng-video-game-db/src/app/app.component.ts
@@ -1,49 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-
-import { GaugeModule } from 'angular-gauge';
-import { MatTabsModule } from '@angular/material/tabs'
-import { MatIconModule } from '@angular/material/icon'
-import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatSelectModule } from '@angular/material/select'
-
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
-import { HttpHeadersInterceptor } from './interceptors/http-headers.interceptor';
-import { HttpErrorsInterceptor } from './interceptors/http-errors.interceptor';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [
-    CommonModule, 
-    RouterOutlet,
-    HttpClientModule,
-    FormsModule,
-    GaugeModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatTabsModule,
-    MatIconModule,
-    SearchBarComponent
-  ],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: HttpHeadersInterceptor,
-      multi: true,
-    },
-    {
-     provide: HTTP_INTERCEPTORS,
-     useClass: HttpErrorsInterceptor,
-     multi: true, 
-    }
-  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-
+  title = 'ng-video-game-db';
 }
