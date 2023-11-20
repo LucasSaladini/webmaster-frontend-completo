8ec6456dec6aaaacbe9e20abd238eb17d06f2113
Lucas Saladini <lucassaladini@hotmail.com>
2023-11-20 11:42:56 -0300

    Fetching game details data

--- JavaScript_Advanced/ng-video-game-db/src/app/components/game-tabs/game-tabs.component.ts
+++ JavaScript_Advanced/ng-video-game-db/src/app/components/game-tabs/game-tabs.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Game } from '../../models';
 
 @Component({
   selector: 'app-game-tabs',
   templateUrl: './game-tabs.component.html',
   styleUrl: './game-tabs.component.scss'
 })
-export class GameTabsComponent {
+export class GameTabsComponent implements OnInit {
+  @Input() game: Game
+  
+  constructor() { }
 
+  ngOnInit(): void {
+    
+  }
 }
